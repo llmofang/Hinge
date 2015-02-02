bc0b72ddae4d34facb90905e424a89ed1f8e7a9e
xuuj <517534219@qq.com>
2015-02-02 11:34:09 +0800

    add okhttp method open_2

--- android-agent/src/main/java/com.llmofang.android.agent/HttpUtil.java
+++ android-agent/src/main/java/com.llmofang.android.agent/HttpUtil.java
@@ -1,7 +1,14 @@
 package com.llmofang.android.agent;
 
-public class HttpUtil{
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
+public class HttpUtil{
+  public static Proxy getProxy()
+  {
+      Proxy proxy = new Proxy(java.net.Proxy.Type.HTTP, new InetSocketAddress("10.0.0.100", 8080));
+      return  proxy;
+  }
 
 
 
--- android-agent/src/main/java/com.llmofang.android.agent/OkHttpInstrumentation.java
+++ android-agent/src/main/java/com.llmofang.android.agent/OkHttpInstrumentation.java
@@ -4,12 +4,14 @@
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
 import com.squareup.okhttp.Request;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.net.URL;
 
 
   public class OkHttpInstrumentation
@@ -34,10 +36,19 @@ import java.net.Proxy;
 //      return null;
 //    }
 
+
+    public static HttpURLConnection open_2(OkUrlFactory factory,URL url)
+    {
+      OkHttpClient client=factory.client();
+      client.setProxy(HttpUtil.getProxy());
+      HttpURLConnection connection= new OkUrlFactory(client).open(url);
+      return connection;
+    }
+
+
     public static Call newCall(OkHttpClient client,Request request)
     {
-      Proxy proxy = new Proxy(java.net.Proxy.Type.HTTP, new InetSocketAddress("10.0.0.10", 8080));
-      client.setProxy(proxy);
+      client.setProxy(HttpUtil.getProxy());
       return client.newCall(request);
     }
   }
